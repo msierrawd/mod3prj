d047e386ea7a4d427880c4acb56cab53c9e599ce
Melvin Sierra <msierrawd@gmail.com>
2021-02-16 14:54:34 -0500

    Implemented methods

--- server/src/main/java/com/example/server/service/DateServiceImpl.java
+++ server/src/main/java/com/example/server/service/DateServiceImpl.java
@@ -1,5 +1,27 @@
 package com.example.server.service;
 
+import com.example.server.model.Date;
+import org.springframework.http.HttpStatus;
+
 public class DateServiceImpl implements DateService {
 
+    @Override
+    public Iterable<Date> getDates() {
+        return null;
+    }
+
+    @Override
+    public Date createDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public Date updateDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public HttpStatus deleteDate(Long id) {
+        return null;
+    }
 }
