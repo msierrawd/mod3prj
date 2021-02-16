3c6afe4f07f92249207c68767f64b9cc3c8da2d7
Melvin Sierra <msierrawd@gmail.com>
2021-02-16 10:37:09 -0500

    Adding id getter and setters

--- server/src/main/java/com/example/server/model/Date.java
+++ server/src/main/java/com/example/server/model/Date.java
@@ -14,6 +14,14 @@ public class Date {
     public Date() {
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Integer getDay() {
         return day;
     }
