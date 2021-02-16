002baa7ba2ee47621303a1fa8160586c24c008a6
Melvin Sierra <msierrawd@gmail.com>
2021-02-16 15:12:53 -0500

    Adding @Service to line 9 in DateServiceImpl to fix error

--- server/src/main/java/com/example/server/service/DateServiceImpl.java
+++ server/src/main/java/com/example/server/service/DateServiceImpl.java
@@ -4,7 +4,9 @@ import com.example.server.model.Date;
 import com.example.server.repository.DateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DateServiceImpl implements DateService {
 
     @Autowired
