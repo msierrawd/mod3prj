b208a0adb339421d8225bd0d6ec0ebb5f45843c3
Melvin Sierra <msierrawd@gmail.com>
2021-02-16 14:58:32 -0500

    Adding DateRepository bean

--- server/src/main/java/com/example/server/service/DateServiceImpl.java
+++ server/src/main/java/com/example/server/service/DateServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.server.service;
 
 import com.example.server.model.Date;
+import com.example.server.repository.DateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 public class DateServiceImpl implements DateService {
 
+    @Autowired
+    DateRepository dateRepository;
     @Override
     public Iterable<Date> getDates() {
         return null;
