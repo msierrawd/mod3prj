da332f761175979d8a4dcb68ab9e0e220b0a66a4
Melvin Sierra <msierrawd@gmail.com>
2021-02-16 14:03:59 -0500

    Added @GetMapping and made getDates()

--- server/src/main/java/com/example/server/controller/DateController.java
+++ server/src/main/java/com/example/server/controller/DateController.java
@@ -1,9 +1,15 @@
 package com.example.server.controller;
 
+import com.example.server.model.Date;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/date")
 public class DateController {
+    @GetMapping
+    public Iterable<Date> getDates(){
+        return dataService.getDates()
+    }
 }
