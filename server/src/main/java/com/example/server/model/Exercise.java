bfc1a6f9041c1f137b26537c57f66d4082784a21
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 09:49:52 -0500

    Creation of Exercise model in model folder as well as creation of variables inside of Exercise.java in model folder

---
+++ server/src/main/java/com/example/server/model/Exercise.java
@@ -0,0 +1,13 @@
+package com.example.server.model;
+
+public class Exercise {
+
+    private String exercise;
+    private Integer set;
+    private Integer reps;
+    private Integer weight;
+    private String lborkg;
+    
+
+
+}
