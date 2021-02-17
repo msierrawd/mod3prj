d5d26a2659d52c4bed45c0021d151271d907f66d
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 11:25:16 -0500

    Adding implements NoteService because I forgot to

--- server/src/main/java/com/example/server/service/NoteServiceImpl.java
+++ server/src/main/java/com/example/server/service/NoteServiceImpl.java
@@ -1,12 +1,13 @@
 package com.example.server.service;
 
+import com.example.server.model.Note;
 import com.example.server.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NoteServiceImpl {
+public class NoteServiceImpl implements NoteService {
     @Autowired
     NoteRepository noteRepository;
-    
+
 }
