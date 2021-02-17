625e3d3030ad6dc07c1e2a19ec23764878fcbf16
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 11:58:54 -0500

    Adding @Autowired NoteService noteService

--- server/src/main/java/com/example/server/controller/NoteController.java
+++ server/src/main/java/com/example/server/controller/NoteController.java
@@ -1,9 +1,15 @@
 package com.example.server.controller;
 
+import com.example.server.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notes")
 public class NoteController {
+    @Autowired
+    NoteService noteService;
+
+    
 }
