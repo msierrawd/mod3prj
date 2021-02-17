ee826681679dbecfe7557c5636b9d993b4c27ecc
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 09:52:16 -0500

    Making id variable because I forgot to make it

--- server/src/main/java/com/example/server/model/Exercise.java
+++ server/src/main/java/com/example/server/model/Exercise.java
@@ -1,13 +1,15 @@
 package com.example.server.model;
 
 public class Exercise {
-
+    private Long id;
     private String exercise;
     private Integer set;
     private Integer reps;
     private Integer weight;
     private String lborkg;
-    
+
+
+
 
 
 }
