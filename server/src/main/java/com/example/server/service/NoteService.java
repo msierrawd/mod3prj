d5dc5f837a4a4948249627b9bb00d2e9b65ef8bb
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 11:18:29 -0500

    Creating methods for future use

--- server/src/main/java/com/example/server/service/NoteService.java
+++ server/src/main/java/com/example/server/service/NoteService.java
@@ -1,5 +1,11 @@
 package com.example.server.service;
 
-public interface NoteService {
+import com.example.server.model.Note;
+import org.springframework.http.HttpStatus;
 
+public interface NoteService {
+    Iterable<Note> getNotes();
+    Note createNote(Note note);
+    Note updateNote(Note note);
+    HttpStatus deleteNote(Long id);
 }
