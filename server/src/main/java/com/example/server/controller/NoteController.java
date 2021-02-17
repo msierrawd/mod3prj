a73a7b81baffe983a1e7ad594584c64d417f398d
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 11:58:13 -0500

    Addition of @RequestMapping('/notes')

--- server/src/main/java/com/example/server/controller/NoteController.java
+++ server/src/main/java/com/example/server/controller/NoteController.java
@@ -1,7 +1,9 @@
 package com.example.server.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/notes")
 public class NoteController {
 }
