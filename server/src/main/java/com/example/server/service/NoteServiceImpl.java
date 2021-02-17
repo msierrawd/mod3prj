bd0ceb1989ad7f48dd6a1d35d54847809cdeaab2
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 11:25:47 -0500

    Implemented methods

--- server/src/main/java/com/example/server/service/NoteServiceImpl.java
+++ server/src/main/java/com/example/server/service/NoteServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.server.service;
 import com.example.server.model.Note;
 import com.example.server.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +11,23 @@ public class NoteServiceImpl implements NoteService {
     @Autowired
     NoteRepository noteRepository;
 
+    @Override
+    public Iterable<Note> getNotes() {
+        return null;
+    }
+
+    @Override
+    public Note createNote(Note note) {
+        return null;
+    }
+
+    @Override
+    public Note updateNote(Note note) {
+        return null;
+    }
+
+    @Override
+    public HttpStatus deleteNote(Long id) {
+        return null;
+    }
 }
