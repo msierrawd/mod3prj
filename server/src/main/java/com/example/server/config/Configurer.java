6986d45f078a449cfeffc7648a0e0ef5375e5b33
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 12:13:49 -0500

    Made Configurer implement WebMvcConfigurer

--- server/src/main/java/com/example/server/config/Configurer.java
+++ server/src/main/java/com/example/server/config/Configurer.java
@@ -2,9 +2,10 @@ package com.example.server.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class Configurer {
+public class Configurer implements WebMvcConfigurer {
 
 }
