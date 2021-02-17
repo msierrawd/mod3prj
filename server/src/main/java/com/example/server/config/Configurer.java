143920b9b8ab5953f1f4d4c724b46c3b0f946058
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 12:13:17 -0500

    Addition of @EnableWebMvc

--- server/src/main/java/com/example/server/config/Configurer.java
+++ server/src/main/java/com/example/server/config/Configurer.java
@@ -1,8 +1,10 @@
 package com.example.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class Configurer {
 
 }
