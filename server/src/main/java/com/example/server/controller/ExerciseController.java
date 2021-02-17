52c2ba196edf14d4b32248ce01d12420ab0ab958
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 10:27:24 -0500

    Adding @Autowired ExerciseService exerciseService

--- server/src/main/java/com/example/server/controller/ExerciseController.java
+++ server/src/main/java/com/example/server/controller/ExerciseController.java
@@ -1,10 +1,14 @@
 package com.example.server.controller;
 
+import com.example.server.service.ExerciseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/exercises")
 public class ExerciseController {
+    @Autowired
+    ExerciseService exerciseService;
     
 }
