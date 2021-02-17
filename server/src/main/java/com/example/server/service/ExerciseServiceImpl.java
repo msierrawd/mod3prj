97aa43ecde3adb42293dad7abb098eac1816e247
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 10:07:26 -0500

    Implementing all methods in ExerciseServiceImpl

--- server/src/main/java/com/example/server/service/ExerciseServiceImpl.java
+++ server/src/main/java/com/example/server/service/ExerciseServiceImpl.java
@@ -1,4 +1,26 @@
 package com.example.server.service;
 
+import com.example.server.model.Exercise;
+import org.springframework.http.HttpStatus;
+
 public class ExerciseServiceImpl implements ExerciseService {
+    @Override
+    public Iterable<Exercise> getExercises() {
+        return null;
+    }
+
+    @Override
+    public Exercise createExercise(Exercise exercise) {
+        return null;
+    }
+
+    @Override
+    public Exercise updateExercise(Exercise exercise) {
+        return null;
+    }
+
+    @Override
+    public HttpStatus deleteExercise(Long id) {
+        return null;
+    }
 }
