645837772aa3a800b9306b569e4b38db23f5fb2c
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 09:54:33 -0500

    Made getter and setters for all variables in Exercise.java model

--- server/src/main/java/com/example/server/model/Exercise.java
+++ server/src/main/java/com/example/server/model/Exercise.java
@@ -11,5 +11,52 @@ public class Exercise {
 
     public Exercise() {
     }
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(String exercise) {
+        this.exercise = exercise;
+    }
+
+    public Integer getSet() {
+        return set;
+    }
+
+    public void setSet(Integer set) {
+        this.set = set;
+    }
+
+    public Integer getReps() {
+        return reps;
+    }
+
+    public void setReps(Integer reps) {
+        this.reps = reps;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getLborkg() {
+        return lborkg;
+    }
+
+    public void setLborkg(String lborkg) {
+        this.lborkg = lborkg;
+    }
 }
