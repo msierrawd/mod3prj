91dc4e9d8869739e41f2776b59d61574e4a374e1
Melvin Sierra <msierrawd@gmail.com>
2021-02-17 11:23:01 -0500

    Adding @Autowired NoteRepository noteRepository

--- server/src/main/java/com/example/server/service/NoteServiceImpl.java
+++ server/src/main/java/com/example/server/service/NoteServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.server.service;
 
+import com.example.server.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteServiceImpl {
+    @Autowired
+    NoteRepository noteRepository;
+    
 }
