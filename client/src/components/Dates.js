eca274a4e541449f33c6eb347614f826fbc67e7d
Melvin Sierra <msierrawd@gmail.com>
2021-02-21 16:29:28 -0500

    Working on create functionality for date

--- client/src/components/Dates.js
+++ client/src/components/Dates.js
@@ -13,9 +13,29 @@ function Dates(){
             console.error(e , e.message);
         }
     }
-        useEffect(() => {
-            getDates();
-        }, [])
+    
+    useEffect(() => {
+        getDates();
+    }, [])
+
+    const [form, setForm] = useState(null);
+
+    function handleChange(e){
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        createDate();
+    }
+
+    async function createDate(){
+        try{
+            const res = await axios.post('http://localhost:8080/dates', form);
+        }catch(e){
+            console.error(e, e.message);
+        }
+    }
 
         return(
             <div>
@@ -24,6 +44,27 @@ function Dates(){
                         <h2> {date.day}/{date.month}/{date.year}</h2>
                     </div>
                 ))}
+
+                <div>
+                    <h2>Create Date</h2>
+                    <form
+                        onChange= { (e) => handleChange(e) }
+                        onSubmit = { (e) => handleSubmit(e) }>
+                        <label>
+                            Day: 
+                            <input type="text" name="day"/>
+                        </label>
+                        <label>
+                            Month: 
+                            <input type="text" name="month"/>
+                        </label>
+                        <label>
+                            Year: 
+                            <input type="text" name="year"/>
+                        </label>
+                        <input type="submit" value="Create Date"/>
+                    </form>
+                </div>
             </div>
         )
 }
