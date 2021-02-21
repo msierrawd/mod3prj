bbea4464fe2f486a7af38bf426db770d941a1e88
Melvin Sierra <msierrawd@gmail.com>
2021-02-21 15:43:51 -0500

    Small code formatting to make it look prettier

--- client/src/components/Dates.js
+++ client/src/components/Dates.js
@@ -3,29 +3,29 @@ import axios from 'axios';
 
 function Dates(){
 
-const [dates, setDates ] = useState(null);
+    const [dates, setDates ] = useState(null);
 
-async function getDates(){
-    try{
-        const res = await axios.get('http://localhost:8080/dates');
-        setDates(res.data);
-    }catch(e){
-        console.error(e , e.message);
+    async function getDates(){
+        try{
+            const res = await axios.get('http://localhost:8080/dates');
+            setDates(res.data);
+        }catch(e){
+            console.error(e , e.message);
+        }
     }
-}
-    useEffect(() => {
-        getDates();
-    }, [])
+        useEffect(() => {
+            getDates();
+        }, [])
 
-    return(
-        <div>
-            { dates && dates.map(date => (
-                <div className= "date" key={ date.id }> 
-                    <h2> {date.day}/{date.month}/{date.year}</h2>
-                </div>
-            ))}
-        </div>
-    )
+        return(
+            <div>
+                { dates && dates.map(date => (
+                    <div className= "date" key={ date.id }> 
+                        <h2> {date.day}/{date.month}/{date.year}</h2>
+                    </div>
+                ))}
+            </div>
+        )
 }
 
 export default Dates; 
