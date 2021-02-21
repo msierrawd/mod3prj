4f3f8c1895354ce553e7a88b03a38090a696e542
Melvin Sierra <msierrawd@gmail.com>
2021-02-21 15:20:52 -0500

    Testing to make sure Dates in components folder is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Dates from './components/Dates';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Dates/>
       </header>
     </div>
   );
--- client/src/components/Dates.js
+++ client/src/components/Dates.js
@@ -1,1 +1,15 @@
 import React, { useEffect, useState } from 'react';
+
+function Dates(){
+    useEffect(() => {
+        console.log("Hello, this is also a test!");
+    })
+
+    return(
+        <div>
+            <h1>Hello, this is a test!</h1>
+        </div>
+    )
+}
+
+export default Dates; 
