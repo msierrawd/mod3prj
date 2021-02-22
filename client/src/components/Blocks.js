6bb479d84127bc739d25c2fe057a09e97b59e4b0
Melvin Sierra <msierrawd@gmail.com>
2021-02-22 11:12:22 -0500

    Making rough wireframe of what Blocks.js will do

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,2 +1,13 @@
 // This component will tie together Dates, Exercises, and Notes
 import React, { useEffect, useState } from 'react';
+
+function Blocks(){
+    return(
+        // Rough wireframe of what Blocks will return
+        <div> 
+            {/* <Dates/> */}
+            {/* <Exercises/> */}
+            {/* <Notes/> */}
+        </div>
+    )
+}
