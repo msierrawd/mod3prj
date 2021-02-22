f4fe3ed8e29d513c5145853f614022ab2d87796b
Melvin Sierra <msierrawd@gmail.com>
2021-02-22 11:04:58 -0500

    Destructuring to make code look prettier

--- client/src/components/Dates.js
+++ client/src/components/Dates.js
@@ -21,7 +21,8 @@ function Dates(){
     const [form, setForm] = useState(null);
 
     function handleChange(e){
-        setForm({ ...form, [e.target.name]: e.target.value });
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
     }
 
     function handleSubmit(e){
