dc520cb4c669a4979e354261ff78a17824c6da72
Melvin Sierra <msierrawd@gmail.com>
2021-02-22 18:01:41 -0500

    Adding small notes

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -3,6 +3,7 @@ import Exercises from './Exercises';
 import Notes from './Notes';
 
 // ADD CSS TO MAKE THINGS LOOK PRETTIER 
+// ALSO TRY AND FIX KEY ISSUE THAT SHOWS UP IN CONSOLE.LOG 
 function Blocks(){
     return(
         <div> 
