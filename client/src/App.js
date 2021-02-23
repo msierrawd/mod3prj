c15c985e334d091bd5a36114eb4578fd36bcb095
Melvin Sierra <msierrawd@gmail.com>
2021-02-23 10:11:42 -0500

    Added a top nav, not sure what all of the code does but at least it works and looks pretty

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Blocks from './components/Blocks'
+import TopNavBar from './components/TopNavBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TopNavBar/>
         <Blocks/>
       </header>
     </div>
---
+++ client/src/components/TopNavBar.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import {  Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    NavbarText} from 'reactstrap'
+
+function TopNavBar(){
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+     
+    return (
+    <div>
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Gym Notepad</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+            <NavItem>
+                <NavLink href="https://github.com/msierrawd/mod3prj">GitHub</NavLink>
+            </NavItem>
+            </Nav>
+            <NavbarText> Have a good workout :)</NavbarText>
+        </Collapse>
+        </Navbar>
+    </div>
+    );
+}
+
+export default TopNavBar;
